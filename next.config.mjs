8d536baefb1cd4467054a5abc2c2b28d0eb8dac0
NIKHIL HAZARIKA <nikhilhazarika9@gmail.com>
2025-11-27 20:49:07 +0530

    chang runtime

--- next.config.mjs
+++ next.config.mjs
@@ -1,37 +1,32 @@
-import withPWA from "next-pwa";
-import runtimeCaching from "next-pwa/cache";
+
+import withPWA from 'next-pwa';
 
 let mydisable;
 let myremoveConsole;
-
-process.env.RUN_MODE == "devlopment"
-  ? (mydisable = true)
-  : (mydisable = false);
-process.env.RUN_MODE == "devlopment"
-  ? (myremoveConsole = false)
-  : (myremoveConsole = true);
+process.env.RUN_MODE=="devlopment"?mydisable=true:mydisable=false
+process.env.RUN_MODE=="devlopment"?myremoveConsole=false:myremoveConsole=true
 
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: false,
   compiler: {
-    removeConsole: myremoveConsole,
+    removeConsole:myremoveConsole,
   },
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "res.cloudinary.com", pathname: "/**" },
-      { protocol: "https", hostname: "images.clerk.dev", pathname: "/**" },
-      { protocol: "https", hostname: "www.gravatar.com", pathname: "/avatar/**" },
+      { protocol: 'https', hostname: 'res.cloudinary.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'images.clerk.dev', pathname: '/**' },
+      { protocol: 'https', hostname: 'www.gravatar.com', pathname: '/avatar/**' },
     ],
   },
 };
 
 export default withPWA({
-  dest: "public",
-  disable: false,
+  dest: 'public',
+  disable:false,
   register: true,
   skipWaiting: true,
-  scope: "/",
-  sw: "sw.js",
-  runtimeCaching,            // <-- ADD THIS LINE
+  scope: '/',
+  sw: 'sw.js',
+   runtimeCaching,
 })(nextConfig);
