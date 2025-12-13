c3882dc8c142d07e3a317ce692e4d2bc15e8922d
NIKHIL HAZARIKA <nikhilhazarika9@gmail.com>
2025-12-13 09:57:40 +0530

    chnge database name

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export  const ConnectDB=()=>{
     mongoose.connect(process.env.MONGO_URL,{
-        dbName:"xihoron_V1",
+        dbName:"xihoronSupabase_V1",
     }).then(c=>console.log(`database connected ${c.connection.host}`)).catch(e=>console.log(e));
    
 };
